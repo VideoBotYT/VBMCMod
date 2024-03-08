9a2d66dad9291775a7b00d5c3238a1c1513e9daf
VideoBot <lander.meersman@gmail.com>
2024-03-08 17:07:25 +0100

    Initial commit

---
+++ src/main/java/net/videobot/videobotmod/VideoBotModClient.java
@@ -0,0 +1,10 @@
+package net.videobot.videobotmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class VideoBotModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/videobot/videobotmod/VideoBotModDataGenerator.java
@@ -0,0 +1,11 @@
+package net.videobot.videobotmod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class VideoBotModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
